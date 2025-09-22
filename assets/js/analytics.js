ff3068bd22b4ad3341ebab6188cc4ecc584e27bc
wangkangpeng <feiyu_wkp@raycloud.com>
2025-09-22 12:16:54 +0800

    fix ga4

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -3,7 +3,7 @@
     // 加载 Google Tag Manager
     const script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ME7HRY1CXN'\;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ME7HRY1CXN';
     document.head.appendChild(script);
 
     // 初始化 gtag
@@ -18,7 +18,7 @@
 (function() {
     const script = document.createElement('script');
     script.async = true;
-    script.src = 'https://analytics.ahrefs.com/analytics.js'\;
+    script.src = 'https://analytics.ahrefs.com/analytics.js';
     script.setAttribute('data-key', 'HDiRWpHWVxttOINcLS9xmA');
     document.head.appendChild(script);
 })();
